737be927c78995161dbfa2c833a9dd7054eaf4bb
Herve Schnegg <herve.schnegg@telegraph.co.uk>
2017-01-23 21:49:15 +0000

    Added tfidf scores for each documents

---
+++ nlp/feature_extraction.py
@@ -0,0 +1,19 @@
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+
+
+__author__ = 'herve.schnegg'
+
+
+def extract_tfidf(text):
+    pipeline = Pipeline([
+        ('pip_vect', CountVectorizer()),
+        ('pip_tfidf', TfidfTransformer()),
+    ])
+    pipeline.fit(text)
+    params = pipeline.get_params()
+    vocabulary = zip(params['pip_vect'].get_feature_names(), params['pip_tfidf'].idf_)
+    tfidf = pipeline.transform(text)
+    return vocabulary, tfidf
+    
--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import conf
 from acquire_text import guardian_open_platform
 from nlp import clean_text
+from nlp import feature_extraction
+import pickle
 
 
 __author__ = 'herve.schnegg'
@@ -21,3 +23,13 @@ corpus = {k: {'article_body': v['article_body'], \
               'article_tags': v['article_tags'], \
               'cleaned_text': clean_text.clean_text(v['article_body'])} \
           for k, v in corpus.items()}
+
+
+# Extract tfidf
+vocabulary, tfidf = feature_extraction.extract_tfidf(map(lambda a: corpus[a]['cleaned_text'], corpus.keys()))
+for k in range(tfidf.shape[0]):
+    corpus[corpus.keys()[k]].update({"tfidf": tfidf[k,:]})
+
+
+# pickle.dump(corpus, open('data/corpus.dictionary.p', 'wb'))
+# corpus = pickle.load(open('data/corpus.dictionary.p', 'rb'))
